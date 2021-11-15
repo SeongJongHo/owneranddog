f776ec946b2bc499d33aae4d1556b4a27242836c
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-15 14:28:55 +0900

    models.py 작성완료

---
+++ informations/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Owner(models.Model):
+  id=models.AutoField(primary_key=True)
+  name=models.CharField(max_length=45)
+  email=models.EmailField(max_length=300)
+  age=models.IntegerField(default=0)
+  class Meta:
+    db_table="owners"
+  def __str__(self):
+    return self.name
+
+class Dog(models.Model):
+  id=models.AutoField(primary_key=True)
+  owner=models.ForeignKey("Owner",on_delete=models.CASCADE)
+  name = models.CharField(max_length=45)
+  age=models.IntegerField(default=0)
+  class Meta:
+    db_table="dogs"
+  def __str__(self):
+    return self.name
+# Create your models here.
