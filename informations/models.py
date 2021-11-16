69f1f6a7ad7d5de8bc6db9255f3ab73f7fe93868
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-16 16:51:45 +0900

    GET,POST 데이터 주고받기 완료

--- informations/models.py
+++ informations/models.py
@@ -5,18 +5,22 @@ class Owner(models.Model):
   name=models.CharField(max_length=45)
   email=models.EmailField(max_length=300)
   age=models.IntegerField(default=0)
+    
   class Meta:
-    db_table="owners"
+      db_table="owners"
+  
   def __str__(self):
-    return self.name
+      return self.name
 
 class Dog(models.Model):
   id=models.AutoField(primary_key=True)
   owner=models.ForeignKey("Owner",on_delete=models.CASCADE)
   name = models.CharField(max_length=45)
   age=models.IntegerField(default=0)
+    
   class Meta:
-    db_table="dogs"
+      db_table="dogs"
+    
   def __str__(self):
-    return self.name
+      return self.name
 # Create your models here.
--- informations/views.py
+++ informations/views.py
@@ -1,26 +1,62 @@
 import json
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
 from .models import Owner,Dog
-from django.http import JsonResponse
 
 class Owner_informationView(View):
+  
   def post(self,request):
     data = json.loads(request.body)
-    for owner_data in data:
-      owner=Owner.objects.create(name=owner_data["owner_name"],age=owner_data["owner_age"]
-      ,email=owner_data["email"])
-    return JsonResponse(status=201)
+    Owner.objects.create(
+      name=data["owner_name"],
+      age=data["owner_age"],
+      email=data["email"])
+
+    return JsonResponse({'MESSAGE':'CREATED'}, status=201)
+
+  def get(self,request):
+   
+    owners=Owner.objects.all()
+    results=[]
+    for owner in owners:
+        dog=owner.dog_set.values("name","age").filter(owner=owner.id)
+        results.append({
+        "dogs":[d for d in dog],
+        # dog_set = Dog.objects
+        # "dogs" : list(owner.dog_set.values('name',"age")),
+        "owner_name" : owner.name,
+        "owner_age" : owner.age,
+        "email" : owner.email,
+        
+        })
+    return JsonResponse({"result":results}, status=200)
 
 class Dog_informationView(View):
+  
   def post(self,request):
+ 
     data = json.loads(request.body)
-    owner=Owner.objects.filter(name=data["owner_name"],email=data["owner_email"])
-    for dog_data in data:
-      dog=Dog.objects.create(name=dog_data["dog_name"],age=dog_data["dog_age"]
-      ,owner=owner[0])
-    return JsonResponse(status=201)
-      
+    owner=Owner.objects.filter(name=data["owner_name"],email=data["email"])
+    
+    Dog.objects.create(name=data["dog_name"],age=data["dog_age"],owner=owner[0])
+   
+    return JsonResponse({'MESSAGE':'CREATED'}, status=201)
+  
+  def get(self,request):
+
+    dogs=Dog.objects.all()
+    results=[]
+    for dog in dogs:
+						results.append(
+							{
+									"dog_name" : dog.name,
+                  "dog_age" : dog.age,
+                  "owner":dog.owner.name
+                  
+							}
+				)
+    return JsonResponse({"result":results}, status=200)
+    
 
 
 # Create your views here.
